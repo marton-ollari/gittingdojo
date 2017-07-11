b3ab085476043f2ec23ed6613386b43b2c33715a
Richárd Szombat <szombatrichard@gmail.com>
2017-07-11 09:40:23 +0200

    joke

--- gittie.py
+++ gittie.py
@@ -1,6 +1,6 @@
 def introduce():
-<<<<<<< HEAD
     print("Hello,I'm Gittie!")
-=======
-    print("Hello, I'm Gittie!")
->>>>>>> 0fd4e318c8173563ee736c9b55c8b758bb80466d
+
+
+def joke():
+    print("This is a joke.")
