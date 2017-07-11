9e0eeec9967b3e1696bb8c5588a68b4d0da06936
Ollári Márton <ollarimarton@gmail.com>
2017-07-11 09:39:48 +0200

    add function

--- gittie.py
+++ gittie.py
@@ -1,6 +1,5 @@
 def introduce():
-<<<<<<< HEAD
-    print("Hello,I'm Gittie!")
-=======
     print("Hello, I'm Gittie!")
->>>>>>> 0fd4e318c8173563ee736c9b55c8b758bb80466d
+
+def add(a,b):
+    return a+b
