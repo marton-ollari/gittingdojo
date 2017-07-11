b9b43f165367633328ae3833f3cf379aef53f549
Ollári Márton <ollarimarton@gmail.com>
2017-07-11 09:50:52 +0200

    functions

--- gittie.py
+++ gittie.py
@@ -1,13 +1,9 @@
 def introduce():
-<<<<<<< HEAD
     print("Hello,I'm Gittie!")
 
 
 def joke():
     print("This is a joke.")
-=======
-    print("Hello, I'm Gittie!")
 
 def add(a,b):
     return a+b
->>>>>>> 9e0eeec9967b3e1696bb8c5588a68b4d0da06936
